5dbf3e69a64f92d2e8a741ea3dcbf1833841be51
khazic <khazzz1c@gmail.com>
2025-09-15 15:55:49 +0800

    kk

--- configs/user_bench_5k.py
+++ configs/user_bench_5k.py
@@ -1,3 +1,5 @@
+import os
+import json
 from mmengine.config import read_base
 
 # Compose multiple built-in datasets and cap test size to 5k
@@ -14,6 +16,77 @@ for _src in (ifeval_datasets, mmlu_datasets, bbh_datasets, gsm8k_datasets, ceval
     for _d in _src:
         _cfg = dict(_d)
         _reader_cfg = dict(_cfg.get('reader_cfg', {}))
-        _reader_cfg['test_range'] = '[:5000]'
-        _cfg['reader_cfg'] = _reader_cfg
-        datasets.append(_cfg)
+    _reader_cfg['test_range'] = '[:5000]'
+    _cfg['reader_cfg'] = _reader_cfg
+    datasets.append(_cfg)
+
+# Build a single HF model from environment variables
+from opencompass.models import (  # noqa: E402
+    HuggingFaceBaseModel,
+    HuggingFacewithChatTemplate,
+)
+
+_hf_path = os.environ.get('OC_HF_PATH', '')
+_abbr = os.environ.get('OC_MODEL_ABBR', os.path.basename(_hf_path.rstrip('/')) or 'hf_model')
+_hf_type = os.environ.get('OC_HF_TYPE', 'chat')
+
+_num_gpus = int(os.environ.get('OC_HF_NUM_GPUS', '1'))
+_batch_size = int(os.environ.get('OC_BATCH_SIZE', '16'))
+_max_seq_len = int(os.environ.get('OC_MAX_SEQ_LEN', '4096'))
+_max_out_len = int(os.environ.get('OC_MAX_OUT_LEN', '1024'))
+
+try:
+    _model_kwargs = json.loads(os.environ.get('OC_MODEL_KWARGS', '{"trust_remote_code": true, "dtype": "bfloat16", "device_map": "auto"}'))
+except Exception:
+    _model_kwargs = {"trust_remote_code": True, "dtype": "bfloat16", "device_map": "auto"}
+try:
+    _generation_kwargs = json.loads(os.environ.get('OC_GENERATION_KWARGS', '{"do_sample": false, "num_beams": 1}'))
+except Exception:
+    _generation_kwargs = {"do_sample": False, "num_beams": 1}
+_tokenizer_path = os.environ.get('OC_TOKENIZER_PATH', None)
+try:
+    _tokenizer_kwargs = json.loads(os.environ.get('OC_TOKENIZER_KWARGS', '{}'))
+except Exception:
+    _tokenizer_kwargs = {}
+_peft_path = os.environ.get('OC_PEFT_PATH', None)
+try:
+    _peft_kwargs = json.loads(os.environ.get('OC_PEFT_KWARGS', '{}'))
+except Exception:
+    _peft_kwargs = {}
+_pad_token_id = os.environ.get('OC_PAD_TOKEN_ID', None)
+try:
+    _stop_words = json.loads(os.environ.get('OC_STOP_WORDS', '[]'))
+except Exception:
+    _stop_words = []
+
+_mod = HuggingFacewithChatTemplate if _hf_type == 'chat' else HuggingFaceBaseModel
+
+models = [
+    dict(
+        type=_mod,
+        abbr=_abbr,
+        path=_hf_path,
+        model_kwargs=_model_kwargs,
+        tokenizer_path=_tokenizer_path,
+        tokenizer_kwargs=_tokenizer_kwargs,
+        peft_path=_peft_path,
+        peft_kwargs=_peft_kwargs,
+        generation_kwargs=_generation_kwargs,
+        max_seq_len=_max_seq_len,
+        max_out_len=_max_out_len,
+        batch_size=_batch_size,
+        pad_token_id=None if _pad_token_id is None else int(_pad_token_id),
+        stop_words=_stop_words,
+        run_cfg=dict(num_gpus=_num_gpus),
+    )
+]
+
+# Avoid dumping module objects in generated config files
+try:
+    del json
+except Exception:
+    pass
+try:
+    del os
+except Exception:
+    pass
